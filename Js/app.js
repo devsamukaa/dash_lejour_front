ef30a3a52551ae8e9f9440bae0ac00e0fcadc3db
dev-dougie <dougllasp.s@outlook.com>
2020-11-07 17:41:59 -0300

    Final Front

---
+++ Js/app.js
@@ -0,0 +1,11 @@
+const btnYear = document.querySelector("#btnYear").addEventListener("click", function(e){
+        e.preventDefault();
+})
+
+const btnBudget = document.querySelector("#btnBudget").addEventListener("click", function(e){
+    e.preventDefault();
+})
+
+const btnStatus = document.querySelector("#btnStatus").addEventListener("click", function(e){
+    e.preventDefault();
+})
